648148d06b993e5213d3734d31adbd8ea9d78b0d
Pkoobz <kristoffen.p@gmail.com>
2024-04-17 10:34:30 +0700

    Update YourLibrary.tsx

--- Spotifyproject/src/pages/YourLibrary.tsx
+++ Spotifyproject/src/pages/YourLibrary.tsx
@@ -1,5 +1,4 @@
 import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonPage, IonSegment, IonSegmentButton, IonSelect, IonSelectOption, IonTitle, IonToggle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 import { search, add } from 'ionicons/icons';
 
@@ -62,7 +61,6 @@ const Tab3: React.FC = () => {
               </IonItem>
             ))}
           </IonList>
-        <ExploreContainer name="Tab 3 page" />
       </IonContent>
     </IonPage>
   );
