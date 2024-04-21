ea80587fc58940435c7ad799d93fae4e1708b205
Jonathan22022 <129663657+Jonathan22022@users.noreply.github.com>
2024-04-21 11:32:52 +0700

    Add files via upload

---
+++ Spotifyproject/src/pages/Lupapassword.tsx
@@ -0,0 +1,43 @@
+import { IonPage, IonHeader, IonContent, IonBackButton, IonToolbar, IonTitle, IonCol, IonGrid, IonRow, IonItem, IonLabel, IonInput, IonButton } from '@ionic/react';
+import React from 'react';
+
+const Lupapassword:React.FC = () => {
+
+    return(
+        <>
+            <IonPage>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonBackButton defaultHref='/login' />
+                        <IonTitle>Lupa Password</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className='ion-padding'>
+                    <IonGrid>
+                        <IonRow>
+                            <IonCol size-sm="8" offset-sm="2" size-md="6" offset-md="3">
+                                <IonGrid className='ion-no-padding'>
+                                    <IonRow>
+                                        <IonCol>
+                                            <IonItem>
+                                                <IonLabel>Masukkan username</IonLabel>
+                                                <IonInput type="text" />
+                                            </IonItem>
+                                        </IonCol>
+                                    </IonRow>
+                                    <IonRow>
+                                        <IonCol>
+                                            <IonButton routerLink='/lupa1'>Konfirm Username</IonButton>
+                                        </IonCol>
+                                    </IonRow>
+                                </IonGrid>
+                            </IonCol>
+                        </IonRow>
+                    </IonGrid>
+                </IonContent>
+            </IonPage>
+        </>
+    );
+};
+
+export default Lupapassword;
---
+++ Spotifyproject/src/pages/Registration.tsx
@@ -0,0 +1,52 @@
+import { IonBackButton, IonButton, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonItemGroup, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+
+const Registration:React.FC = () =>{
+    return(
+        <>
+            <IonPage>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonBackButton defaultHref='/login' />
+                        <IonTitle>Buat akun</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className='ion-padding'>
+                    <IonGrid>
+                        <IonRow>
+                            <IonCol size-sm="8" offset-sm="2" size-md="6" offset-md="3">
+                                <IonGrid className='ion-no-padding'>
+                                    <IonRow>
+                                        <IonCol>
+                                            <IonItemGroup>
+                                                <IonItem>
+                                                    <IonLabel>Masukkan username yang diinginkan</IonLabel>
+                                                    <IonInput type="text" />
+                                                </IonItem>
+                                                <IonItem>
+                                                    <IonLabel>Masukkan password yang diinginkan</IonLabel>
+                                                    <IonInput type="text" /> 
+                                                </IonItem>
+                                                <IonItem>   
+                                                    <IonLabel>Konfirmasi password</IonLabel> 
+                                                    <IonInput type="text" />
+                                                </IonItem>
+                                            </IonItemGroup>
+                                        </IonCol>
+                                    </IonRow>
+                                    <IonRow>
+                                        <IonCol>
+                                            <IonButton shape="round">Daftarkan</IonButton>
+                                        </IonCol>
+                                    </IonRow>
+                                </IonGrid>
+                            </IonCol>
+                        </IonRow>
+                    </IonGrid>
+                </IonContent>
+            </IonPage>
+        </>
+    );
+}
+
+export default Registration;
