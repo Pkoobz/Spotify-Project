20babed78aac6e0f4732819880cc03b499de7dff
Jonathan22022 <129663657+Jonathan22022@users.noreply.github.com>
2024-05-10 21:55:42 +0700

    Update Lupapassword.tsx

--- Spotifyproject/src/pages/Lupapassword.tsx
+++ Spotifyproject/src/pages/Lupapassword.tsx
@@ -1,42 +1,47 @@
-import { IonPage, IonHeader, IonContent, IonBackButton, IonToolbar, IonTitle, IonCol, IonGrid, IonRow, IonItem, IonLabel, IonInput, IonButton } from '@ionic/react';
-import React from 'react';
+import { IonPage, IonHeader, IonContent, IonBackButton, IonToolbar, IonTitle, IonCol, IonGrid, IonRow, IonItem, IonLabel, IonInput, IonButton, IonButtons, IonList } from '@ionic/react';
+import React, { FormEvent, useRef, useState } from 'react';
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
 
 const Lupapassword:React.FC = () => {
+    const [email, setEmail] = useState('')
 
+    const handlePassword = async () => {
+      await sendPasswordResetEmail(auth, email)
+        .then(()=> alert("password reset email sent"))
+        .catch((error:any)=>console.log(error.message));
+    }
     return(
         <>
             <IonPage>
-                <IonHeader>
-                    <IonToolbar>
-                        <IonButtons slot='start'>  
-                            <IonBackButton defaultHref='/login' />
-                        </IonButtons>
-                        <IonTitle>Lupa Password</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <IonContent className='ion-padding'>
-                    <IonGrid>
-                        <IonRow>
-                            <IonCol size-sm="8" offset-sm="2" size-md="6" offset-md="3">
-                                <IonGrid className='ion-no-padding'>
-                                    <IonRow>
-                                        <IonCol>
-                                            <IonItem>
-                                                <IonLabel>Masukkan username</IonLabel>
-                                                <IonInput type="text" />
-                                            </IonItem>
-                                        </IonCol>
-                                    </IonRow>
-                                    <IonRow>
-                                        <IonCol>
-                                            <IonButton routerLink='/lupa1'>Konfirm Username</IonButton>
-                                        </IonCol>
-                                    </IonRow>
-                                </IonGrid>
-                            </IonCol>
-                        </IonRow>
-                    </IonGrid>
-                </IonContent>
+              <IonHeader>
+                <IonToolbar>
+                  <IonButtons slot='start'>
+                    <IonBackButton defaultHref='/login' />
+                  </IonButtons>
+                  <IonTitle>Lupa Password</IonTitle>
+                </IonToolbar>
+              </IonHeader>
+              <IonContent>
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <IonList>
+                        <IonItem>
+                          <IonLabel position="floating">Masukkan Email</IonLabel>
+                          <IonInput value={email} type="email" onIonChange={(e:any) => setEmail(e.target.value)} />
+                        </IonItem>
+                      </IonList>
+                    </IonCol>
+                  </IonRow>
+                  <IonRow>
+                    <IonCol>
+                      <IonButton onClick={handlePassword}>Kirimkan</IonButton>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonContent>
             </IonPage>
         </>
     );
