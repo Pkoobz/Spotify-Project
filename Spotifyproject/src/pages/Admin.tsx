c18fc6e7fe2b5cf6b4c1c317496c90bb8befb242
Jonathan22022 <129663657+Jonathan22022@users.noreply.github.com>
2024-05-12 00:02:46 +0700

    Update Admin.tsx

--- Spotifyproject/src/pages/Admin.tsx
+++ Spotifyproject/src/pages/Admin.tsx
@@ -8,6 +8,7 @@ import { getStorage, uploadBytes, ref, getDownloadURL } from 'firebase/storage';
 const Admin:React.FC = () => {
     const db = getFirestore();
     const [artists, setArtists] = useState<Array<any>>([]);
+    const [lagus, setLagu] = useState<Array<any>>([]);
 
     useEffect(() => {
         async function getData() {
@@ -21,10 +22,31 @@ const Admin:React.FC = () => {
         getData();
     }, [db]);
 
+    useEffect(() => {
+        async function getData() {
+            try {
+                const querySnapshot0 = await getDocs(collection(db, "lagu"));
+                setLagu(querySnapshot0.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+            } catch (error) {
+                console.error("Error getting documents: ", error);
+            }
+        }
+        getData();
+    }, [db]);
+
     async function deleteArtist(artistId: string) {
         try {
             await deleteDoc(doc(db, "artists", artistId));
             setArtists(artists.filter(artist => artist.id !== artistId));
+        } catch (error) {
+            console.error("Error removing document: ", error);
+        }
+    }
+
+    async function deleteLagu(laguId: string) {
+        try {
+            await deleteDoc(doc(db, "lagu", laguId));
+            setLagu(lagus.filter(lagu => lagu.id !== laguId));
         } catch (error) {
             console.error("Error removing document: ", error);
         }
@@ -65,6 +87,35 @@ const Admin:React.FC = () => {
                                                                 <td>{artist.foto}</td>
                                                                 <td><IonButton routerLink={`/admin/${artist.id}`}>Edit</IonButton></td>
                                                                 <td><IonButton onClick={() => deleteArtist(artist.id)}>Delete</IonButton></td>
+                                                            </tr>
+                                                        ))}
+                                                    </tbody>
+                                                </table>
+                                            </IonCol>
+                                    </IonRow>
+                                    <IonRow>
+                                        <IonCol>
+                                            <IonButton routerLink='/addsong'>Add Song</IonButton>
+                                                <table border={1}>
+                                                    <tbody>
+                                                        <tr>
+                                                            <th>No</th>
+                                                            <th>Nama</th>
+                                                            <th>Artis</th>
+                                                            <th>Foto</th>
+                                                            <th>Lagu</th>
+                                                            <th>Edit</th>
+                                                            <th>Delete</th>
+                                                        </tr>
+                                                        {lagus.map((lagu, index) => (
+                                                            <tr key={lagu.id}>
+                                                                <td>{index + 1}</td>
+                                                                <td>{lagu.namalagu}</td>
+                                                                <td>{lagu.namaartist}</td>
+                                                                <td>{lagu.foto0}</td>
+                                                                <td>{lagu.foto1}</td>
+                                                                <td><IonButton routerLink={`/admin/${lagu.id}`}>Edit</IonButton></td>
+                                                                <td><IonButton onClick={() => deleteLagu(lagu.id)}>Delete</IonButton></td>
                                                             </tr>
                                                         ))}
                                                     </tbody>
